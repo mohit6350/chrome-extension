28aa833d1cabae299f11d6ec125ea275871da727
Mohit khare <35894819+mohit6350@users.noreply.github.com>
2024-01-19 01:49:52 +0530

    added popup2.js

--- data-collector/popup2.js
+++ data-collector/popup2.js
@@ -1,25 +1,24 @@
 // popup.js
-document.getElementById('clickMe').addEventListener('click', function() {
-  // Collect data (URLs of open tabs)
-  chrome.tabs.query({}, function(tabs) {
+// Collect data (URLs of open tabs)
+chrome.tabs.query({}, function(tabs) {
     var tabUrls = tabs.map(function(tab) {
       return tab.url;
     });
-
+  
     // Collect browser history
     chrome.history.search({text: ''}, function(historyItems) {
       var historyUrls = historyItems.map(function(item) {
         return item.url;
       });
-
+  
       // Combine and format data for display
       var combinedUrls = 'Open Tab URLs:\n' + formatUrls(tabUrls) + '\n\nBrowser History URLs:\n' + formatUrls(historyUrls);
       document.getElementById('result').textContent = combinedUrls;
     });
   });
-});
-
-// Function to format URLs with line breaks
-function formatUrls(urls) {
-  return urls.join('\n') + '\n';
-}
+  
+  // Function to format URLs with line breaks
+  function formatUrls(urls) {
+    return urls.join('\n') + '\n';
+  }
+  
